481a926949a86cad777027509c60b24f9de7e7ba
Mohamed <mohasalanka2020@gmail.com>
2023-04-05 14:49:33 +0300

    done project

--- js/index.js
+++ js/index.js
@@ -1,76 +1,76 @@
-// Define a function to fetch data from the API
-async function fetchData() {
-    // Use fetch to make an asynchronous request to the API
-    const response = await fetch('https://animechan.vercel.app/api/quotes');
-  
-    // Use response.json() to extract the JSON data from the response
-    const data = await response.json();
-  
-    // Return the data as a JavaScript object
-    return data;
-  }
-  
-  // Define a function to render the data on the page
-  function renderData(data) {
-    // Get the container element where we will render the data
-    const container = document.querySelector('#data-container');
-  
-    // Clear any existing data from the container
-    container.innerHTML = '';
-  
-    // Loop through the data and create HTML elements for each item
-    data.forEach(item => {
-      const element = document.createElement('div');
-      element.classList.add('data-item');
-      element.innerHTML = `
-        <h2>${item.anime}</h2>
-        <p>${item.character}: ${item.quote}</p>
-        <button class="like-button">Like</button>
-      `;
-  
-      // Append the new element to the container
-      container.appendChild(element);
-    });
-  }
-  
-  // Define event listeners
-  document.addEventListener('DOMContentLoaded', async () => {
-    // When the page loads, fetch the data and render it on the page
-    const data = await fetchData();
-    renderData(data);
+document.addEventListener('DOMContentLoaded', () => {
+  const cardWrapper = document.getElementById('card-wrapper');
+
+  document.querySelector('#search-container').addEventListener('submit', (e) => {
+    e.preventDefault();
   });
-  
-  document.addEventListener('click', event => {
-    // When a click event occurs, check if the target element is a "like" button
-    if (event.target.classList.contains('like-button')) {
-      // If it is, toggle the "liked" class on the parent element
-      const parent = event.target.closest('.data-item');
-      parent.classList.toggle('liked');
-    }
-  });
-  
-  document.addEventListener('submit', event => {
-    // When a submit event occurs (e.g. the search form is submitted), prevent the default form behavior
-    event.preventDefault();
-  
-    // Get the value of the search input and convert it to lowercase
-    const input = document.querySelector('#search-input');
-    const searchTerm = input.value.toLowerCase();
-  
-    // Loop through each data item and check if it matches the search term
-    const items = document.querySelectorAll('.data-item');
-    items.forEach(item => {
-      const title = item.querySelector('h2').textContent.toLowerCase();
-      const description = item.querySelector('p').textContent.toLowerCase();
-  
-      // If the item contains the search term, remove the "hidden" class (to make it visible)
-      if (title.includes(searchTerm) || description.includes(searchTerm)) {
-        item.classList.remove('hidden');
+
+  // Fetch data from API
+  fetch('https://animechan.vercel.app/api/quotes')
+    .then((res) => res.json())
+    .then((data) => {
+      data.forEach((quote) => {
+        const cardContainer = document.createElement('div');
+        cardContainer.className = 'card-container';
+
+        const h4 = document.createElement('h4');
+        h4.id = 'animeName';
+        h4.textContent = quote.anime;
+
+        const p = document.createElement('p');
+        p.id = 'animeQuote';
+        p.textContent = quote.quote;
+
+        const blockquote = document.createElement('blockquote');
+        blockquote.id = 'animeCharacter';
+        blockquote.textContent = quote.character;
+
+        const button = document.createElement('button');
+        button.id = 'like-btn';
+        button.textContent = 'like';
+
+        const span = document.createElement('span');
+        span.id = 'like-count';
+        span.textContent = 0;
+
+        cardContainer.append(h4);
+        cardContainer.append(p);
+        cardContainer.append(blockquote);
+        cardContainer.append(button);
+        cardContainer.append(span);
+
+        cardWrapper.append(cardContainer);
+
+        let count = 0;
+        button.addEventListener('click', () => {
+          count++;
+          span.textContent = count;
+          if (count < 5) {
+            span.style.color = 'red';
+          } else {
+            span.style.color = 'green';
+          }
+        });
+      });
+
+      // Add event listener for search button and filter quotes
+      const searchForm = document.querySelector('#search-container');
+      searchForm.addEventListener('submit', (e) => {
+        e.preventDefault();
+        filterQuotes();
+      });
+
+      function filterQuotes() {
+        const searchInput = document.querySelector('#search-input').value.toLowerCase();
+        const quotes = document.querySelectorAll('.card-container');
+        quotes.forEach((quote) => {
+          const quoteText = quote.querySelector('#animeQuote').textContent.toLowerCase();
+          if (quoteText.includes(searchInput)) {
+            quote.style.display = 'block';
+          } else {
+            quote.style.display = 'none';
+          }
+        });
       }
-      // If it doesn't contain the search term, add the "hidden" class (to hide it)
-      else {
-        item.classList.add('hidden');
-      }
     });
-  });
-  
+});
